2097b23fc9aa8714d6d6ab5be27954393bb9d4e9
huachen <hc0507@126.com>
2022-04-02 09:56:53 +0800

    demo

--- app/src/main/java/com/hc/flowlayout/FiveActivity.java
+++ app/src/main/java/com/hc/flowlayout/FiveActivity.java
@@ -3,12 +3,25 @@ package com.hc.flowlayout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class FiveActivity extends AppCompatActivity {
-
+    MyView2 myView2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_five);
+        myView2 = findViewById(R.id.myview);
+        myView2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(myView2.isScan()){
+                    Log.d("FiveActivity", "scan");
+                }else {
+                    Log.d("FiveActivity", "noscan");
+                }
+            }
+        });
     }
 }
